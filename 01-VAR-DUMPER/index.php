ac946d4da3c10c3e1aa65407e3c483c530e4a609
Abdel760 <16972@csmrouen.net>
2018-02-19 10:47:53 +0100

    Introduction Symfony

--- 01-VAR-DUMPER/index.php
+++ 01-VAR-DUMPER/index.php
@@ -2,3 +2,31 @@
 
 # Importation de l'Autoload de Composer
 require_once 'vendor/autoload.php';
+
+
+# Contenu de Démonstration
+class Contact {
+    private $_firstname,
+            $_lastname;
+
+    /**
+     * Contact constructor.
+     * @param $_firstname
+     * @param $_lastname
+     */
+    public function __construct($_firstname, $_lastname)
+    {
+        $this->_firstname = $_firstname;
+        $this->_lastname = $_lastname;
+    }
+}
+
+$unString = 'Une chaîne de caractères';
+$unArray  = ['Abdel-illah', 'Hocine', 'Benjamin', 'Clément', 'Romain', 'Joffrey', 'Jean-Marc'];
+$unObjet  = new Contact('Abdel-illah', 'BOUKHATEB');
+
+# Test de VarDumper
+\Symfony\Component\VarDumper\VarDumper::dump($unString);
+\Symfony\Component\VarDumper\VarDumper::dump($unArray);
+\Symfony\Component\VarDumper\VarDumper::dump($unObjet);
+
